148cf604c7b8f28b46218097f1dca1dd60812d74
Kobayashi-Yukari <0609mucc@gmail.com>
2022-10-05 19:34:43 +0900

    フロント周りの実装

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,56 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
+{{--Font Awesome読み込みリンク--}}
+<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+<head>
+
+<!-- Scripts -->
+<!--jQuery/toastrt読み込み-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+    // {{--トーストオプション--}}
+toastr.options = {
+  "positionClass": "toast-top-right",
+  "timeOut": "1200",
+};
+@if (session('msg_success'))
+	$(function () {
+	    toastr.success('{{ session('msg_success') }}');
+	});
+    @endif
+@if (session('msg_error'))
+	$(function () {
+	    toastr.error('{{ session('msg_error') }}');
+	});
+    @endif
+</script>
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>M_chanel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
+@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Bebas+Neue&display=swap');
             html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
+font-family: 'M PLUS Rounded 1c', sans-serif;
+                background-color:  #1c273e;
+                color: #efeeea;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
             }
-
+.m-b-md {
+		font-family: 'Bebas Neue', cursive;
+}
             .full-height {
                 height: 100vh;
             }
@@ -46,7 +76,7 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 80px;
             }
 
             .links > a {
@@ -62,34 +92,73 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+.btn-flat-border {
+  display: inline-block;
+  padding: 0.3em 1em;
+  text-decoration: none;
+  color: #efeeea;
+  border: solid 2px #efeeea;
+  border-radius: 3px;
+  transition: .4s;
+margin: 30px;
+font-family: 'Arial', sans-serif;
+}
+
+.btn-flat-border:hover {
+  background: #efeeea;
+  color: #1c273e;
+}
+details {
+    padding: .5em .5em 0;
+text-align: left;
+}
+
+summary {
+    margin: -.5em -.5em 0;
+    padding: .5em;
+}
+
+details[open] {
+    padding: .5em;
+}
+
+details[open] summary {
+    margin-bottom: .5em;
+}
+.details_p {
+font-size: 13px;
+}
+
+
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+<div class="container">
+<div class="row">
+<div class="col-md-8 col-md-offset-2">
+<div class="panel panel-default">
+<div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    M_CHANEL
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+<p id="example">はじめまして、M_chanelの管理人Mです。<br>
+ここはムッカーである管理人『 M 』が運営するMUCC非公式サイトです。<br><br>
+</p>
+<a href="{{ route('login') }}" class="btn-flat-border">ログイン</a><br>
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/top', 'PostController@index')->name('post.index');
+Route::get('/post/create', 'PostController@create')->name('post.create');
+Route::post('/post/store', 'PostController@store')->name('post.store');
+Route::get('/post/edit/{id}', 'PostController@edit')->name('post.edit');
+Route::get('/user/index', 'UserController@index')->name('user.index');
+Route::post('/post/update', 'PostController@update')->name('post.update');
+Route::get('/post/destroy/{id}', 'PostController@destroy')->name('post.destroy');
+
+//ファンレター関連
+Route::get('/letter/index', 'LetterController@index')->name('letter.index');
+Route::get('/letter/create', 'LetterController@create')->name('letter.create');
+Route::get('/letter/edit/{id}', 'LetterController@edit')->name('letter.edit');
+Route::post('/letter/update', 'LetterController@update')->name('letter.update');
+Route::post('/letter/preview', 'LetterController@preview')->name('letter.preview');
+Route::get('/letter/store/{id}', 'LetterController@store')->name('letter.store');
+Route::get('/letter/clancel/{id}', 'LetterController@cancel')->name('letter.cancel');
