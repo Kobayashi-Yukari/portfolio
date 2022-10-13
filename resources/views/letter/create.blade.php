7b952e9eaec0ea913304ed120fc0f8092e266de2
Kobayashi-Yukari <0609mucc@gmail.com>
2022-10-13 21:37:08 +0900

    画像のバグ修正

--- resources/views/letter/create.blade.php
+++ resources/views/letter/create.blade.php
@@ -59,7 +59,7 @@ margin-left: 10px;
 <ul style="list-style: none;">
 <li><img style="border-radius: 35%; height: 65px; width: 65px;" src="{{ asset('storage/avatars/' . Auth::user()->avatar) }}">
 <input type="radio" name="letter_avatar" value="1" checked @if(old('letter_avatar') == "1") checked @endif> 自分のアイコン</li><br>
-<li><img style="border-radius: 35%; height: 65px; width: 65px;" src="{{ asset('storage/avatars/' . '07.png') }}">
+<li><img style="border-radius: 35%; height: 65px; width: 65px;" src="{{ asset('storage/avatars/' . 'default.png') }}">
 <input type="radio" name="letter_avatar" value = "2" @if(old('letter_avatar') == "2") checked @endif> 匿名さま専用アイコン</li>
 </ul>
 </div>
